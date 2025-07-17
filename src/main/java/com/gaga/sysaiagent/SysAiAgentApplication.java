84020ef54eba2dd70464fdef4d1a440a2f11338a
Zakariae ESSAIYDY <zakaressaiydy@gmail.com>
2025-07-17 15:43:42 +0100

    first commit

---
+++ src/main/java/com/gaga/sysaiagent/SysAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.gaga.sysaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysAiAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaga/sysaiagent/SysAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaga.sysaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
